b63aecf35b930add7a96b6f015f2f92747b0109f
nikita-sharov <nikita.sharov@235u.net>
2020-02-27 15:20:27 +0100

    Add project files

---
+++ CartoonMagazine/README.md
@@ -0,0 +1,13 @@
+# Publishing a digital/mobile fist management cartoon magazine
+
+See [job details](docs/job-details.pdf) (ongoing project project, fixed-price), as posted on February 24, 2020 within `Mobile Design`, including following images [renamed] posted as attachments,
+
+- [Not Found](docs/not-found.jpg)
+- [Russian Intelligence HQ](docs/russian-intelligence-hq.jpg)
+- [Social Media Marketing](docs/social-media-marketing.jpg)
+
+asking for expertise in `Mobile UI Design`, `User Experienmce Design`, and `Visual Design`.
+
+## Solution
+
+Live at https://cartoon-magazine.azurewebsites.net, [administration area](https://cartoon-magazine.azurewebsites.net/manager) credentials are `admin` / `password`.
---
+++ CartoonMagazine/src/CartoonMagazine/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Piranha;
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.AttributeBuilder;
+using Piranha.Data.EF.SQLite;
+using Piranha.Manager.Editor;
+
+namespace CartoonMagazine
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Service setup
+            services.AddPiranha(options =>
+            {
+                options.AddRazorRuntimeCompilation = true;
+
+                options.UseFileStorage();
+                options.UseImageSharp();
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseMemoryCache();
+                options.UseEF<SQLiteDb>(db =>
+                    db.UseSqlite(Configuration.GetConnectionString("piranha")));
+                options.UseIdentityWithSeed<IdentitySQLiteDb>(db =>
+                    db.UseSqlite(Configuration.GetConnectionString("piranha")));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Initialize Piranha
+            App.Init(api);
+
+            // Configure cache level
+            App.CacheLevel = Piranha.Cache.CacheLevel.Basic;
+
+            // Build content types
+            new ContentTypeBuilder(api)
+                .AddAssembly(typeof(Startup).Assembly)
+                .Build()
+                .DeleteOrphans();
+
+            // Configure Tiny MCE
+            EditorConfig.FromFile("editorconfig.json");
+
+            // Middleware setup
+            app.UsePiranha(options =>
+            {
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseIdentity();
+            });
+        }
+    }
+}
